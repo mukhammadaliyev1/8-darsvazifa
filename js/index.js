ee14bac41d09188c70be12295fcad59bb8c79dc6
mukhammadaliyev1 <muhammadnurmamadaliyev335@gmail.com>
2024-06-21 10:39:29 +0500

    3/1 bold

---
+++ js/index.js
@@ -0,0 +1,113 @@
+    //  ASosiy metodlarga oid misollar
+   //  1-misol
+    
+// let arr =[1, 2,34, 5, 6,7];
+
+// function lengthArr(arr){
+//     return arr.length;
+// }
+// console.log(lengthArr(arr));
+
+
+  // 2-misol 
+
+//   let arr =[1, 2,34, 5, 6,7];
+//    function tostringAr(arr){
+//     let res = arr.toString();
+//     return res;
+//    }
+//    console.log(tostringAr(arr));
+
+
+    // 3-misol
+
+    // let arr =[1, 2,34, 5, 6,7];
+    // function tojoinAr(arr){
+    //     let res = arr.join(", ")
+    //     return res;
+    // }
+    // console.log(tojoinAr(arr));
+
+
+    // 4-misol
+
+    // let arr =[1, 2,34, 5, 6,7];
+
+    // function newElement(arr, element){
+    //     arr.push(element);
+    //     return arr;
+    // }
+    // console.log(newElement(arr, 0));
+     
+
+    // 5-misol
+    
+    // let arr =[1, 2,34, 5, 6,7];
+
+    //  function deleteLasElement(arr){
+    //     arr.pop();
+    //     return arr;
+    //  }
+    //  console.log(deleteLasElement(arr));
+
+
+    // 6-misol
+
+    // let arr =[1, 2,34, 5, 6,7];
+    
+    // function deletefistElement(arr, element){
+    //         arr.shift(element);
+    //         return arr;
+    //     }
+    //     console.log(deletefistElement(arr));
+
+
+    // 7-misol
+
+
+    // let arr =[1, 2,34, 5, 6,7];
+    // function unshiftElement(arr, element){
+    //              arr.unshift(element);
+    //              return arr;
+    //          }
+    //          console.log(unshiftElement(arr,0));
+    
+
+
+
+    // 8-misol
+
+
+    // let arr =[1, 2,34, 5, 6,7];
+    // let arr1 =[1, 2,34, 5, 6,7];
+    // function addTwoElement(arr, arr1){
+    //     let res = arr.concat(arr1);
+    //     return res;
+    // }
+    // console.log(addTwoElement(arr,arr1));
+
+    // 9-misol
+
+    // let arr =[1, 2,34, 5, 6,7];
+
+    // function spliceAr(arr){
+    //     arr.splice(1,2, 111);
+    //     return arr;
+    // }
+    // console.log(spliceAr(arr));
+
+    
+    // 10-msiol
+
+    // let arr =[1, 2,34, 5, 6,7];
+
+    //  function sliceAr(arr){
+    //      let res=   arr.slice(1,3);
+    //         return res;
+    //     }
+    //     console.log(sliceAr(arr));
+    
+
+    // 11-misol
+
+    
