1a7f1cc56fee1dae7ba43462c69e8c19555ef471
mukhammadaliyev1 <muhammadnurmamadaliyev335@gmail.com>
2024-06-21 20:39:52 +0500

    oz qold

--- js/index.js
+++ js/index.js
@@ -14,6 +14,7 @@
 //   let arr =[1, 2,34, 5, 6,7];
 //    function tostringAr(arr){
 //     let res = arr.toString();
+
 //     return res;
 //    }
 //    console.log(tostringAr(arr));
@@ -110,4 +111,189 @@
 
     // 11-misol
 
+
+// let arr = [ "damas" , " cobalt ", "malibu" , "soilov"]
     
+// function lengthArr(arr){
+//     return arr.length;
+// }
+// console.log(lengthArr(arr));
+
+
+// 12-misol
+
+
+
+//   let arr =[1, 2,34, 5, 6,7];
+//    function tostringAr(arr){
+//     let res = arr.toString();
+
+//     return res;
+//    }
+//    console.log(tostringAr(arr));
+
+
+    // 13-misol
+
+    // let arr =[1, 2,34, 5, 6,7];
+    // function tojoinAr(arr){
+    //     let res = arr.join(" ")
+    //     return res;
+    // }
+    // console.log(tojoinAr(arr));
+   
+   
+    // 14-misol
+     
+    
+//     let arr =[1, 2,34, 5, 6,7];
+//     function addtwoElement(arr, element1, element2){
+//         arr.push(element1,element2);
+//         return arr;
+//     }
+// console.log(addtwoElement(arr, 1,2));
+
+
+//   15-misol
+
+  
+
+    // let arr =[1, 2,34, 5, 6,7];
+
+    //  function deletetwoLasElement(arr){
+    //   arr.pop();
+    //   arr.pop();
+    //     return arr;
+    //  }
+    //  console.log(deletetwoLasElement(arr));
+
+    // 16-misol
+
+    
+    // let arr =[1, 2,34, 5, 6,7];
+    
+    // function deletefistElement(arr, element, element1){
+    //         arr.shift(element);
+    //         arr.shift(element1);
+    //         return arr;
+    //     }
+    //     console.log(deletefistElement(arr));
+
+
+
+    // 17-misol
+
+    //  let arr =[1, 2,34, 5, 6,7];
+    
+    //  function unshiftElement(arr, element, element1){
+    //              arr.unshift(element);
+    //              arr.unshift(element1);
+    //              return arr;
+    //          }
+    //          console.log(unshiftElement(arr,0,1111));
+    
+
+
+    // 18-misol
+
+
+    // let arr =[1, 2,34, 5, 6,7];
+    // let arr1 =[12, 24,354, 56, 67,79];
+    // let arr2 =[10, 20,340, 50, 60,70];
+    // function plusthreeElement(arr,arr1,arr2){
+       
+    //    let res= arr.concat(arr1, arr2);
+    //      return res;
+    // }
+    // console.log(plusthreeElement(arr,arr2,arr1));
+
+
+    // 19-misol
+
+    // let arr =[1, 2,34, 5, 6,7];
+    // function deleteElement(arr){
+    //     arr.splice(0 , 1);
+    //     return arr;
+    // }
+    // console.log(deleteElement(arr));
+
+
+    // 20-misol
+
+      
+    // let arr =[1, 2,34, 5, 6,7];
+
+    //  function sliceAr(arr){
+    //      let res=   arr.slice(0,arr.length-1);
+    //         return res;
+    //     }
+    //     console.log(sliceAr(arr));
+    
+
+
+
+
+    // Search metodlariga oid masalalar.
+
+
+    // 1-misol
+
+    // let arr =[1, 2,34, 5, 6,7];
+    // function findElement(arr){
+    //    let result= arr.includes(0);
+    //     return result ;
+    // }
+    // console.log(findElement(arr));
+
+
+
+    // 2-misol
+
+    // let arr =[1, 2,34, 5, 6,7];
+
+    // function fimdIndexElement(arr){
+    // let result=arr.indexOf(1);
+    // return result;
+    // }console.log(fimdIndexElement(arr));
+    
+
+
+
+    // 3-misol 
+
+    
+    // let arr =[1, 2,34, 5, 6,7];
+
+    // function fimdIndexElement(arr){
+    // let result=arr.lastIndexOf(1);
+    // return result;
+    // }console.log(fimdIndexElement(arr));
+    
+
+
+    // 4-misol
+
+
+    
+                // let arr = [2, 4, 56, 6,47,8];
+
+                // function findElEMENT(arr){
+                // let result = arr.find(function(value, index){
+                //     return   value > 2 && value%2==0
+                // })}
+                // console.log(findElEMENT(arr));
+    
+
+                
+                // 5-misol
+
+                // let arr = [2, 4, 56, 6,47,8];
+
+                // function arrFindindex(arr){
+                //  let res=   arr.findIndex(function(value,index){
+                //      return   value>0
+
+                //     })
+                //     return res;
+                // }
+                // console.log(arrFindindex(arr));
